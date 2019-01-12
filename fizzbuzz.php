50baa21f2aeb1c819a10daad22dde7c3431d3c60
andybelyx <46009570+andybelyx@users.noreply.github.com>
2019-01-12 13:50:38 +0200

    Create fizzbuzz.php

---
+++ fizzbuzz.php
@@ -0,0 +1,18 @@
+<?php
+$handle = fopen ("php://stdin","r");
+$fizz = fgets($handle);
+$buzz = fgets($handle);
+$num3 = fgets($handle);
+$i = 0;
+while ($i++ < $num3) {
+	if (($i % (int)$fizz == 0) && ($i % (int)$buzz == 0)) {
+	print "FB ";
+}elseif ($i % (int)$buzz == 0) {
+	print "B ";
+}elseif ($i % (int)$fizz == 0) {
+	print "F ";
+}else {
+	print "$i ";
+}
+}
+?>

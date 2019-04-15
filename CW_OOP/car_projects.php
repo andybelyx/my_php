153b52f48643979808380fe7f431012ad310af7d
php <php@localhost.localdomain>
2019-04-15 14:42:48 +0300

    classWork

--- CW_OOP/car_projects.php
+++ CW_OOP/car_projects.php
@@ -1,10 +1,10 @@
 <?php
 abstract class Car {
-		
-		function __construct($fuel="", $power=0, $gear="") {
+		function __construct($fuel="", $power=0, $gear="", $bodyStyle="") {
 		$this->fuel = $fuel;
 		$this->power = $power;
 		$this->gear = $gear;
+		$this->bodyStyle = $bodyStyle;	
 	}
 }
 
@@ -20,6 +20,8 @@ class Engine extends Car {
 }
 
 class Gear extends Engine {
+private $wheelDrive = "FWD";
+
 	public function tuningGear($newGear) {
 		$this->gear = $newGear;
 		return $this->gear;
@@ -37,13 +39,20 @@ class Gear extends Engine {
             	return $this->$var;
         	}
     }
+	public function __get($wheelDrives) {
+		if ($wheelDrives == "wheelDrive") {
+			return $this->wheelDrive;
+		}
+	}
+
 }
 
-abstract class Info extends Gear {
-
+abstract class Info extends Gear {	
 		function __toString()  {
 		$info = "Car: " . get_class($this) . PHP_EOL . "Color: " . "{$this->getColor()}" . PHP_EOL;
-		$info .= "It's fuel consists of: {$this->fuel}" . PHP_EOL;
+		$info .= "Body style: " . "{$this->bodyStyle}" . PHP_EOL;
+		$info .= "Types of drive: " . "{$this->wheelDrive}" . PHP_EOL;
+		$info .= "It's fuel consists of: " . "{$this->fuel}" . PHP_EOL;
 		$info .= "Engine power is " . "({$this->power})hp" . PHP_EOL;
 		$info .= "Gear: " . "{$this->gear}. " . "Has " . "({$this->getGearCount()})" . " gears" . PHP_EOL;
 		$info .= str_repeat("_",30).PHP_EOL;
@@ -51,20 +60,25 @@ abstract class Info extends Gear {
 	}
 }
 
-class Priora extends Info {
+class Lada_Priora extends Info {
 
 }
 
-class Prius extends Info {
+class Toyota_Prius extends Info {
+
+}
+
+class BMW_M5 extends Info {
 
 }
 
 $engine_2_0Turbo = 200;
 $engine_W12_6l = 450;
+$engine_V8_4_4Bi_Turbo = 600;
 $nos = 150;
 
 
-$priora = new Priora("Propan, Gas", 104, "Mechanic");
+$priora = new Lada_Priora("Propan, Gas", 104, "Mechanic", "Sedan");
 $priora->setColor('Eggplant');
 $priora->tuningEngine($engine_W12_6l);
 $priora($nos);
@@ -72,11 +86,19 @@ $priora->setGearCount(6);
 $priora->tuningGear('Automat');
 echo $priora;
 
-$prius = new Prius("Gas, Electro", 121, "Automat");
+$prius = new Toyota_Prius("Gas, Electro", 121, "Automat", "Hatchback");
 $prius->setColor('White');
 $prius->tuningEngine($engine_2_0Turbo);
 $prius($nos);
 $prius->setGearCount(7);
 $prius->tuningGear('Tiptronic');
 echo $prius;
+
+$BMWm5 = new BMW_M5("Gas", 340, "Automat", "Sedan");
+$BMWm5->setColor('Black');
+$BMWm5->wheelDrive = ("AWD");
+$BMWm5->tuningEngine($engine_V8_4_4Bi_Turbo);
+$BMWm5($nos);
+$BMWm5->setGearCount(7);
+echo $BMWm5;
 
